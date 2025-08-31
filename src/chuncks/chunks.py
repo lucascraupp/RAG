6e0e48b9fdc131ac5fbe2d66fb59f603bf956def
Lucas Coelho Raupp <lucascoelhoraupp@gmail.com>
2025-08-31 14:30:41 -0300

    fix: Corrige inconsistências nos parâmetros da função get_best_method

--- src/chuncks/chunks.py
+++ src/chuncks/chunks.py
@@ -10,24 +10,29 @@ from src.chuncks.text_splitters import (
 )
 
 
-def get_best_method(file_name: str) -> None:
+def get_best_method(filename: str) -> None:
     supabase = create_client(Config.SUPABASE_URL, Config.SUPABASE_KEY)
     response = (
         supabase.table("documents")
         .select("content")
-        .eq("file_name", file_name)
+        .eq("file_name", filename)
         .execute()
     )
 
     if not response.data:
-        logger.info(f"Documento {file_name} não encontrado na base de dados")
+        logger.info(f"Documento {filename} não encontrado na base de dados")
         return
 
-    text_chunks = character_text_splitter(response.data[0]["content"])
-    recursive_text_chunks = recursive_text_splitter(response.data[0]["content"])
+    text_chunks = character_text_splitter(
+        response.data[0]["content"], filename=filename
+    )
+    recursive_text_chunks = recursive_text_splitter(
+        response.data[0]["content"], filename=filename
+    )
     recursive_character_chunks = recursive_character_text_splitter(
-        response.data[0]["content"]
+        response.data[0]["content"], filename=filename
     )
-    markdown_header_splitter = markdown_header_metadata_splitter(
-        response.data[0]["content"]
+    markdown_header_chunks = markdown_header_metadata_splitter(
+        response.data[0]["content"], filename=filename
     )
+    semantic_chunks = semantic_splitter(response.data[0]["content"], filename=filename)
