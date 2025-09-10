4c480b893422191f0a5c0a4b3d76b45c31469047
Lucas Coelho Raupp <lucascoelhoraupp@gmail.com>
2025-09-10 15:36:59 -0300

    refactor: Simplifica a função get_best_method para usar um loop sobre os splitters

--- src/chuncks/chunks.py
+++ src/chuncks/chunks.py
@@ -13,26 +13,34 @@ from src.chuncks.text_splitters import (
 def get_best_method(filename: str) -> None:
     supabase = create_client(Config.SUPABASE_URL, Config.SUPABASE_KEY)
     response = (
-        supabase.table("documents")
-        .select("content")
-        .eq("file_name", filename)
-        .execute()
+        supabase.table("documents").select("content").eq("filename", filename).execute()
     )
 
     if not response.data:
         logger.info(f"Documento {filename} não encontrado na base de dados")
         return
 
-    text_chunks = character_text_splitter(
-        response.data[0]["content"], filename=filename
-    )
-    recursive_text_chunks = recursive_text_splitter(
-        response.data[0]["content"], filename=filename
-    )
-    recursive_character_chunks = recursive_character_text_splitter(
-        response.data[0]["content"], filename=filename
-    )
-    markdown_header_chunks = markdown_header_metadata_splitter(
-        response.data[0]["content"], filename=filename
-    )
-    semantic_chunks = semantic_splitter(response.data[0]["content"], filename=filename)
+    splitters = [
+        character_text_splitter,
+        recursive_text_splitter,
+        recursive_character_text_splitter,
+        markdown_header_metadata_splitter,
+        semantic_splitter,
+    ]
+
+    for splitter in splitters:
+        data = (
+            supabase.table("chunks")
+            .select("*")
+            .eq("strategy", splitter.__name__)
+            .eq("filename", filename)
+            .execute()
+        )
+
+        if data.data:
+            logger.info(f"Chunks já existem para o método '{splitter.__name__}'")
+        else:
+            logger.info(f"Criando chunks usando o método '{splitter.__name__}'")
+            text_chunks = splitter(response.data[0]["content"], filename=filename)
+            supabase.table("chunks").insert(text_chunks).execute()
+            logger.info(f"Chunks criados")
