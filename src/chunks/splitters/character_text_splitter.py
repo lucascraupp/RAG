6c160a07356513450786e0a0caf9f674ba1bc985
Lucas Coelho Raupp <lucascoelhoraupp@gmail.com>
2025-09-26 15:40:55 -0300

    feat: Adiciona classe CustomCharacterTextSplitter para divisão de texto

---
+++ src/chunks/splitters/character_text_splitter.py
@@ -0,0 +1,30 @@
+import logging
+from typing import Dict, List, Optional
+
+from langchain_text_splitters import CharacterTextSplitter
+
+from src.chunks.splitters import TextSplitter
+
+
+class CustomCharacterTextSplitter(TextSplitter):
+    def __init__(self, params: Optional[Dict] = None):
+        super().__init__("character_text_splitter", params)
+
+    def split_text(self, text: str, filename: str) -> List[str]:
+        splitter = CharacterTextSplitter(**self._params)
+
+        logger = logging.getLogger("langchain_text_splitters.base")
+        original_level = logger.level
+
+        try:
+            logger.setLevel(logging.ERROR)
+
+            chunks = splitter.split_text(text)
+
+            self._chunks_data = self._add_headers_to_chunks(
+                text, chunks, filename, self._params.get("chunk_overlap", 0)
+            )
+        finally:
+            logger.setLevel(original_level)
+
+        return chunks
